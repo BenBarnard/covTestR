7fa2d75840cbdba07840bc33a5e71fc20238823d
BenBarnard <ben_barnard@baylor.edu>
2016-07-12 11:55:18 -0500

    seperated covariance functions to their own file

---
+++ R/covariance_functions.R
@@ -0,0 +1,32 @@
+#' Overall Covariance Matirx (helper function)
+#'
+#' @param A1 sum of squares for group 1
+#' @param A2 sum of squares for group 2
+#' @param n1 sample size for group 1
+#' @param n2 sample size for group 2
+#'
+#' @keywords internal
+#'
+overall_cov_func <- function(A1, A2, n1, n2){
+  (1 / (n1 + n2 - 2)) * (A1 + A2)
+}
+
+#' Sum of Squares (helper function)
+#'
+#' @param matrix data matrix
+#'
+#' @keywords internal
+#'
+A_func <- function(matrix){
+  (t(matrix) - colMeans(matrix)) %*% t(t(matrix) - colMeans(matrix))
+}
+
+#' D diagonal matrix for Srivastava 2014 (helper funciton)
+#'
+#' @param matrix data matrix
+#'
+#' @keywords internal
+#'
+Di_func <- function(matrix){
+  diag(matrix %*% t(matrix))
+}
