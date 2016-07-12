fa5103980b09df0188194398bce46f3eb15491fd
BenBarnard <ben_barnard@baylor.edu>
2016-07-12 11:54:37 -0500

    seperated b functions to their own file

---
+++ R/b_functions.R
@@ -0,0 +1,10 @@
+#' b hat used in method introduced by Chaipitak 2013 (helper function)
+#'
+#' @param ahat21 see function
+#' @param ahat22 see function
+#'
+#' @keywords internal
+#'
+bhat_func <- function(ahat21, ahat22){
+  ahat21 / ahat22
+}
