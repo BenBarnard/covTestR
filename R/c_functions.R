41f54d83d669c32c661cc7a5e375f1bb9d2de276
BenBarnard <ben_barnard@baylor.edu>
2016-07-15 21:45:51 -0600

    added @export

--- R/c_functions.R
+++ R/c_functions.R
@@ -4,6 +4,8 @@
 #'
 #' @keywords internal
 #'
+#' @export
+#'
 c0_func <- function(n){
   n * ((n ^ 3) + 6 * (n ^ 2) + 21 * n + 18)
 }
@@ -13,6 +15,8 @@ c0_func <- function(n){
 #' @param n sample size
 #'
 #' @keywords internal
+#'
+#' @export
 #'
 c1_func <- function(n){
   2 * n * (2 * (n ^ 2) + 6 * n + 9)
@@ -24,6 +28,8 @@ c1_func <- function(n){
 #'
 #' @keywords internal
 #'
+#' @export
+#'
 c2_func <- function(n){
   2 * n * (3 * n + 2)
 }
@@ -33,6 +39,8 @@ c2_func <- function(n){
 #' @param n sample size
 #'
 #' @keywords internal
+#'
+#' @export
 #'
 c3_func <- function(n){
   n * (2 * (n ^ 2) + 5 * n + 7)
