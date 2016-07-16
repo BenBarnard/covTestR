682412aa0d73480380cbb74d13900f0bc25261b6
BenBarnard <ben_barnard@baylor.edu>
2016-07-15 21:46:32 -0600

    added @export

--- R/tau_functions.R
+++ R/tau_functions.R
@@ -4,6 +4,8 @@
 #'
 #' @keywords internal
 #'
+#' @export
+#'
 tau_func <- function(n1, n2){
   n <- n1 + n2 - 2
   ((n ^ 5) * ((n ^ 2) + n + 2)) / ((n + 1) * (n + 2) * (n + 4) * (n + 6) * (n - 1) * (n - 3))
