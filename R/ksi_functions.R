1ec687051ab305fb2842c9a64355ec16cb9758f4
BenBarnard <ben_barnard@baylor.edu>
2016-07-15 21:46:27 -0600

    added @export

--- R/ksi_functions.R
+++ R/ksi_functions.R
@@ -9,6 +9,8 @@
 #'
 #' @keywords internal
 #'
+#' @export
+#'
 ksihat2i_func <- function(n, p, ahat1, ahat2, ahat3, ahat4){
   (4 / ((n - 1) ^ 2)) * (((ahat2 ^ 2) / (ahat1 ^ 4)) +
                            ((2 * (n - 1)) / p) *
