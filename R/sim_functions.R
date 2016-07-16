34fd767e92a622c6a9d2443fec804538ea56069b
BenBarnard <ben_barnard@baylor.edu>
2016-07-15 21:37:55 -0600

    reindent and added code

--- R/sim_functions.R
+++ R/sim_functions.R
@@ -16,7 +16,7 @@ critical_value_sim.data.frame <- function(data, ...){
             dlply(.(Group), dataDftoMatrix))
 }
 
-critical_value_sim <- function(data, tests){
+critical_value_sim.matrix <- function(data, tests){
   matrix_ls <- data %>%
     dlply(.(Group), dataDftoMatrix)
   n <- matrix_ls %>% llply(function(matrix){
@@ -32,6 +32,39 @@ critical_value_sim <- function(data, tests){
   overall_cov <- (1 / (n[[1]] + n[[2]] - 2)) * (A_ls[[1]] + A_ls[[2]])
   ahat2 <- ahat2_func(n[[1]], n[[2]], p[[1]], overall_cov)
   ahat2i <- cbind(n, p, sample_covs) %>% mlply(ahat2i_func)
+  ahat1 <- ahat1_func(p[[1]], overall_cov)
+  ahat4 <- ahat4_func(A_ls[[1]], A_ls[[2]], p[[1]], n[[1]], n[[2]], ahat2, ahat1)
+  etahat2i <- cbind(n, p, ahat4, ahat2) %>% mlply(etahat2i_func)
+  tau <- tau_func(n[[1]], n[[2]])
+  ahatStar4 <- ahatStar4_func(tau, p[[1]], overall_cov, n[[1]], n[[2]])
+  deltahat2 <- deltahat2_func(ahatStar4, p[[1]], ahat2, n[[1]], n[[2]])
+  bhat <- bhat_func(ahat2i[[1]], ahat2i[[2]])
+  D_ls <- matrix_ls %>% llply(Di_func)
+  overall_n <- n[[1]] + n[[2]]
+  ahat2i_Srivastava2014 <- cbind(n, p, D_ls, A_ls) %>%
+    mlply(function(n, p, D_ls, A_ls){
+      ahat2iSrivastava2014_func(n, p, D_ls, A_ls)
+    })
+  ahat2_Srivastava2014 <- ahat2Srivastava2014_func(ahat2i[[1]], ahat2i[[2]], n[[1]], n[[1]])
+  ahat2_ls <- ahat2i
+  ahat1_ls <- cbind(p, sample_covs) %>% mlply(function(p, sample_covs){
+    ahat1_func(p, sample_covs)
+  })
+  ahat3 <- ahat3_func(A_ls[[1]], A_ls[[2]], p[[1]], n[[1]], n[[2]], ahat2, ahat1)
+  ahat4 <- ahat4_func(A_ls[[1]], A_ls[[2]], p[[1]], n[[1]], n[[2]], ahat2, ahat1)
+  ksihat2_ls <- cbind(n, p, ahat1, ahat2, ahat3, ahat4) %>% mlply(ksihat2i_func)
+  gammahat_ls <- cbind(ahat2_ls, ahat1_ls) %>% mlply(function(ahat2_ls, ahat1_ls){
+    gammahati_func(ahat2_ls, ahat1_ls)
+  })
 
-  Schott2007_test(n, p, ahat2, ahat2i, sample_covs)
+  list(
+    Schott2007 = Schott2007_test(n, p, ahat2, ahat2i, sample_covs),
+    Chaipitak2013 = Chaipitak2013_test(bhat, deltahat2),
+    Srivastava2007 = Srivastava2007_test(ahat2i[[1]], ahat2i[[2]],
+                                         etahat2i[[1]], etahat2i[[2]]),
+    Srivastava2014 = Srivastava2014_test.default(
+      n, p, ahat2_Srivastava2014, ahat2i_Srivastava2014, sample_covs),
+    SrivastavaYanagihara2010 = SrivastavaYanagihara2010_test.default(
+      gammahat_ls[[1]], gammahat_ls[[2]], ksihat2_ls[[1]], ksihat2_ls[[2]])
+  )
 }
