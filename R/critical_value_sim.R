7ddfa5b35dc07ab09d33c1bab8c034bd871f4d51
BenBarnard <ben_barnard@baylor.edu>
2016-07-18 21:41:36 -0500

    update sim

--- R/critical_value_sim.R
+++ R/critical_value_sim.R
@@ -3,19 +3,22 @@
 #' @param data the data
 #' @param ... other stuff
 #'
+#' @importFrom magrittr %>%
+#' @importFrom plyr laply
 #'
 #' @export
 #'
 #' @keywords internal
 #'
-#' @examples critical_value_sim(list(mean = c(0,0,0),
-#'                                   cov = diag(1, 3),
+#' @examples critical_value_sim(list(meanVec = c(0,0,0),
+#'                                   covMat = diag(1, 3),
 #'                                   samples = 10,
-#'                                   populations = 2),
-#'                              10000, .95, Chaipitak2013_test)
+#'                                   pops = 2),
+#'                              100, .95, Chaipitak2013_test)
 critical_value_sim <- function(params, replications, quantile, test){
-
-  replicate(replications)
+  replicate(replications, do.call(mcSamples, params), simplify = FALSE) %>%
+    laply(test) %>%
+    Bp_func(quantile)
 }
 
 
