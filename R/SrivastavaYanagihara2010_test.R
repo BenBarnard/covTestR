3299d2a24184b37b34736f51b796a5459b2dafa3
BenBarnard <ben_barnard@baylor.edu>
2016-10-23 21:56:19 -0500

    update

--- R/SrivastavaYanagihara2010_test.R
+++ R/SrivastavaYanagihara2010_test.R
@@ -7,7 +7,7 @@
 #'
 #' @export
 #'
-#' @examples SrivastavaYanagihara2010_test(mcSamples(c(0,0,0), diag(1, 3), 10, 2, matrix = FALSE, tidy = TRUE), group = population, variables = variable, samples = samples, value = value, tidy = TRUE)
+#' @examples SrivastavaYanagihara2010_test(mcSamples(c(0,0,0), diag(1, 3), 10, 2), group = population)
 #'
 SrivastavaYanagihara2010_test <- function(data, ...){
   UseMethod("SrivastavaYanagihara2010_test")
@@ -17,19 +17,10 @@ SrivastavaYanagihara2010_test <- function(data, ...){
 #'
 #' @importFrom lazyeval expr_find
 #'
-SrivastavaYanagihara2010_test.data.frame <- function(x, group, ..., variables, samples, value, tidy = FALSE){
-  if(tidy == TRUE){
-    tidyDataDftoMatrix(data = x,
-                       group = expr_find(group),
-                       variables = expr_find(variable),
-                       samples = expr_find(samples),
-                       value = expr_find(value),
-                       test = expr_find(SrivastavaYanagihara2010_test.matrix))
-  }else{
+SrivastavaYanagihara2010_test.data.frame <- function(x, group, ...){
     dataDftoMatrix(data = x,
                    group = expr_find(group),
                    test = expr_find(SrivastavaYanagihara2010_test.matrix))
-  }
 }
 
 #' @export
