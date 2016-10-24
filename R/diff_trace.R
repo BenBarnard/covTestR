f85259cb9aa20903dc48e077cf7bd14973928838
BenBarnard <ben_barnard@baylor.edu>
2016-10-23 21:56:25 -0500

    update

--- R/diff_trace.R
+++ R/diff_trace.R
@@ -8,7 +8,7 @@
 #' @return Difference of Traces for Covariance Matrices
 #' @export
 #'
-#' @examples diff_trace(mcSamples(c(0,0,0), diag(1, 3), 10, 2, matrix = FALSE, tidy = TRUE), group = population, variables = variable, samples = samples, value = value, tidy = TRUE)
+#' @examples diff_trace(mcSamples(c(0,0,0), diag(1, 3), 10, 2), group = population)
 #'
 diff_trace <- function(data, ...) {
   UseMethod("diff_trace")
@@ -18,19 +18,10 @@ diff_trace <- function(data, ...) {
 #'
 #' @importFrom lazyeval expr_find
 #'
-diff_trace.data.frame <- function(x, group, ..., variables, samples, value, tidy = FALSE){
-  if(tidy == TRUE){
-    tidyDataDftoMatrix(data = x,
-                       group = expr_find(group),
-                       variables = expr_find(variable),
-                       samples = expr_find(samples),
-                       value = expr_find(value),
-                       test = expr_find(diff_trace.matrix))
-  }else{
-    dataDftoMatrix(data = x,
-                   group = expr_find(group),
-                   test = expr_find(diff_trace.matrix))
-  }
+diff_trace.data.frame <- function(x, group, ...){
+  dataDftoMatrix(data = x,
+                 group = expr_find(group),
+                 test = expr_find(diff_trace.matrix))
 }
 
 #' @export
