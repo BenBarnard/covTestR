987984bfe505d5f8d90b5af015822dde450aa32b
BenBarnard <ben_barnard@baylor.edu>
2016-10-23 21:56:50 -0500

    update

--- R/mcSamples.R
+++ R/mcSamples.R
@@ -20,23 +20,16 @@
 #'
 #' @examples
 #'
-mcSamples <- function(meanVec, covMat, samples, pops, ..., matrix = FALSE, tidy = FALSE){
+mcSamples <- function(meanVec, covMat, samples, pops, ..., matrix = FALSE){
   pop_list <- pop_lists(meanVec, covMat, samples, pops)
   if(matrix == TRUE){
     llply(pop_list, function(list){
       mvrnorm(n = list$samples, mu = list$meanVec, Sigma = list$covMat)
     })
   }else{
-    if(tidy == FALSE){
-      ldply(pop_list, function(list){
-        mvrnorm(n = list$samples, mu = list$meanVec, Sigma = list$covMat)
-      }, .id = "population")
-    }else{
-      melt(ldply(pop_list, function(list){
-        cbind(as.data.frame(mvrnorm(n = list$samples, mu = list$meanVec, Sigma = list$covMat)),
-              samples = 1:samples)
-      }, .id = "population"), id = c("population", "samples"))
-    }
+    ldply(pop_list, function(list){
+      mvrnorm(n = list$samples, mu = list$meanVec, Sigma = list$covMat)
+    }, .id = "population")
   }
 }
 
@@ -56,9 +49,9 @@ mcSamples <- function(meanVec, covMat, samples, pops, ..., matrix = FALSE, tidy 
 #'
 pop_lists <- function(meanVec, covMat, samples, pops){
   params <- llply(seq(pops), function(pop, meanVec, covMat, samples){
-    list(meanVec = if(is.list(meanVec)){meanVec[pop]}else{meanVec},
-         covMat = if(is.list(covMat)){covMat[pop]}else{covMat},
-         samples = if(is.list(samples)){samples[pop]}else{samples})
+    list(meanVec = if(is.list(meanVec)){meanVec[[pop]]}else{meanVec},
+         covMat = if(is.list(covMat)){covMat[[pop]]}else{covMat},
+         samples = if(is.list(samples)){samples[[pop]]}else{samples})
   }, meanVec = meanVec, covMat = covMat, samples = samples)
   setNames(params, 1:length(params))
 }
