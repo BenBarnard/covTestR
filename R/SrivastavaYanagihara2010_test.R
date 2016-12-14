3eb22b1f6bcc5b9eb80b9b3026838c8459f16bb9
BenBarnard <ben_barnard@baylor.edu>
2016-12-14 10:08:48 -0600

    added groupe_df

--- R/SrivastavaYanagihara2010_test.R
+++ R/SrivastavaYanagihara2010_test.R
@@ -25,6 +25,16 @@ SrivastavaYanagihara2010_test.data.frame <- function(x, group, ...){
 
 #' @export
 #'
+#' @importFrom lazyeval expr_find
+#'
+SrivastavaYanagihara2010_test.grouped_df <- function(x, ...){
+  dataDftoMatrix(data = x,
+                 group = attributes(x)$vars[[1]],
+                 test = expr_find(SrivastavaYanagihara2010_test.matrix))
+}
+
+#' @export
+#'
 #' @importFrom lazyeval lazy_dots
 #' @importFrom lazyeval lazy_eval
 #' @importFrom stringr str_detect
