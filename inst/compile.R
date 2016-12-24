fa1182b9461a7b934a155fd65ec0edabd268c920
BenBarnard <ben_barnard@baylor.edu>
2016-12-23 21:15:50 -0700

    update

--- inst/compile.R
+++ inst/compile.R
@@ -10,37 +10,38 @@ power_func <- function(x, critical_values){
   pop <- unique(x$populations)
   te <- unique(x$test)
   sa <- unique(x$Samples)
+  od <- unique(x$originaldimensions)
 
-  critical_value <- filter(critical_values, ReductionMethod == redMeth, type == ty,
+  critical_value <- filter(critical_values, ReductionMethod == redMeth, type == ty, originaldimensions = od,
                            ReducedDimension == redDim, populations == pop, test == te,
                            Samples == sa)$Crit
 
   data.frame(power = mean(x$value >= critical_value))
 }
 
-files <- list.files("~/Box Sync/Dissert Data/100/boxesM")
+files <- list.files("~/Box Sync/Dissert Data/bigsim/elliptical")
 
 data <- ldply(files, function(x){
   filegroup <- as.data.frame(str_split(x, " ", simplify = TRUE))
   names(filegroup) <- c("dimension", "samples", "difference", "type", "extension")
-  data <- read_csv(paste0("~/Box Sync/Dissert Data/100/boxesM/", x))
+  data <- read_csv(paste0("~/Box Sync/Dissert Data/bigsim/elliptical/", x))
   cbind(data, data.frame(Samples = rep(filegroup$samples, nrow(data))))
   })
 
 critical_data <- filter(data, difference == 0)
 
-critical_values <- summarise(group_by(critical_data, ReductionMethod, type,
+critical_values <- summarise(group_by(critical_data, ReductionMethod, type, originaldimensions,
                                       ReducedDimension, populations, Samples, test),
-                             Crit = quantile(value, probs = .95))
+                             Crit = quantile(value, probs = .95, na.rm = TRUE))
 
 power_data <- filter(data, !(difference == 0))
 
-power <- ddply(power_data, .variables = c("ReductionMethod", "type",
+power <- ddply(power_data, .variables = c("ReductionMethod", "type", "originaldimensions",
                                           "ReducedDimension", "populations",
                                           "test", "difference", "Samples"),
                .fun = power_func, critical_values = critical_values)
 
-write_csv(power, "~/Box Sync/Dissert Data/power/15.csv")
+write_csv(power, "~/Box Sync/Dissert Data/power/bigsimEllipticalAdd.csv")
 
 power <- read_csv("~/Box Sync/Dissert Data/power/15.csv")
 
