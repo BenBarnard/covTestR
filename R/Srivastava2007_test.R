f6a90daad727be28a789c620b65c1dc301492b72
BenBarnard <ben_barnard@baylor.edu>
2017-03-26 22:47:59 -0500

    added covariance useMethod

--- R/Srivastava2007_test.R
+++ R/Srivastava2007_test.R
@@ -26,7 +26,7 @@ Srivastava2007_test <- function(x, ...){
 Srivastava2007_test.data.frame <- function(x, group, ...){
   dataDftoMatrix(data = x,
                  group = expr_find(group),
-                 method = expr_find(Srivastava2007_test.matrix),
+                 method = expr_find(Srivastava2007_test.list),
                  .dots = lazy_dots(...))
 }
 
@@ -37,7 +37,7 @@ Srivastava2007_test.data.frame <- function(x, group, ...){
 Srivastava2007_test.grouped_df <- function(x, ...){
   dataDftoMatrix(data = x,
                  group = attributes(x)$vars[[1]],
-                 method = expr_find(Srivastava2007_test.matrix),
+                 method = expr_find(Srivastava2007_test.list),
                  .dots = lazy_dots(...))
 }
 
@@ -48,7 +48,7 @@ Srivastava2007_test.grouped_df <- function(x, ...){
 Srivastava2007_test.resample <- function(x, ...){
   dataDftoMatrix(data = as.data.frame(x),
                  group = attributes(x)$vars[[1]],
-                 method = expr_find(Srivastava2007_test.matrix),
+                 method = expr_find(Srivastava2007_test.list),
                  .dots = lazy_dots(...))
 }
 
@@ -61,22 +61,40 @@ Srivastava2007_test.resample <- function(x, ...){
 #' @importFrom stats cov
 #' @importFrom stats pchisq
 #'
-Srivastava2007_test.matrix <- function(...){
+Srivastava2007_test.list <- function(x, ...){
+
   ls <- lazy_dots(...)
-  matrix_ls <- lazy_eval(ls[str_detect(names(ls), "x.")])
-  names(matrix_ls) <- str_replace(names(matrix_ls), "x.", "")
+  matrix_ls <- x
 
-  ns <- lapply(matrix_ls, function(matrix){
-    nrow(matrix)
-  })
+  if(!("covariance" %in% class(x[[1]])) & ("matrix" %in% class(x[[1]]))){
+    ns <- lapply(matrix_ls, function(matrix){
+      nrow(matrix)
+    })
 
-  p <- lapply(matrix_ls, function(matrix){
-    ncol(matrix)
-  })
+    p <- lapply(matrix_ls, function(matrix){
+      ncol(matrix)
+    })
 
-  A_ls <- lapply(matrix_ls, A_func)
+    A_ls <- lapply(matrix_ls, A_func)
 
-  sample_covs <- lapply(matrix_ls, cov)
+    sample_covs <- lapply(matrix_ls, cov)
+  }
+
+  if("covariance" %in% class(x[[1]])){
+    ns <- lapply(matrix_ls, function(matrix){
+      attributes(matrix)$n
+    })
+
+    p <- lapply(matrix_ls, function(matrix){
+      ncol(matrix)
+    })
+
+    sample_covs <- matrix_ls
+
+    A_ls <- mapply(function(sample_covs, ns){
+      sample_covs * (ns - 1)
+    }, sample_covs = sample_covs, ns = ns, SIMPLIFY = FALSE)
+  }
 
   overall_cov <- overall_cov_func(A_ls, ns)
 
