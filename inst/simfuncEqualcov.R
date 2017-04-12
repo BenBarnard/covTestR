fb5fc4fbef094205d78046c8d7849f907f36df6b
BenBarnard <ben_barnard@baylor.edu>
2017-04-12 15:03:12 -0500

    update

--- inst/simfuncEqualcov.R
+++ inst/simfuncEqualcov.R
@@ -1,8 +1,10 @@
-sim <- function(covarianceMat, replicationscrit, replicationspower, samples, dimensions, differences, directory, fileindex){
+sim <- function(covarianceMat, replicationscrit, replicationspower, samples, dimensions, differences, directory){
 
   source(system.file("functions.R", package = "EqualCov"))
 
+files <- list.files(directory)
 
+if(!("critical_tests.RData" %in% files)){
   critical_tests <- ldply(samples,
                          function(x, covarianceMat, replicationscrit, dimensions){
                            ldply(replicate(
@@ -12,25 +14,44 @@ sim <- function(covarianceMat, replicationscrit, replicationspower, samples, dim
                          }, covarianceMat = covarianceMat,
                          replicationscrit = replicationscrit,
                          dimensions = dimensions)
+  save(critical_tests, file = paste0(directory, "critical_tests.RData"))
+}else{
+  load(paste0(directory, "critical_tests.RData"))
+}
 
-  save(critical_tests, file = paste0(directory, "critical_tests", fileindex, ".RData"))
 
+if(!("critical_values.RData" %in% files)){
   critical_values <- summarize(group_by(critical_tests, Dimensions, Samples, Test, Populations), `Critical Value` = quantile(Value, 0.95))
 
-  save(critical_values, file = paste0(directory, "critical_values", fileindex, ".RData"))
+  save(critical_values, file = paste0(directory, "critical_values.RData"))
+}else{
+  load(paste0(directory, "critical_values.RData"))
+}
 
-  powertestgrid <- expand.grid(Samples = samples, Differences = differences)
+powertestgrid <- expand.grid(Samples = samples, Differences = differences)
+
+
+if(!("power_tests.RData" %in% files)){
 
   power_tests <- mdply(powertestgrid,
                        .fun = powerdata,
                        covarianceMat = covarianceMat, replicationspower = replicationspower, dimensions = dimensions)
 
   save(power_tests, file = paste0(directory, "power_tests", fileindex, ".RData"))
+}else{
+  load(paste0(directory, "power_tests.RData"))
+browser()
+}
 
+if(!("power_tests.RData" %in% files)){
   power_values <- ddply(.data = power_tests, .variables = .(Samples, Dimensions, Differences, Test, Populations),
                         .fun = powervalue, .progress = progress_text(char = "u"), critValues = critical_values)
 
   save(power_values, file = paste0(directory, "power_values", fileindex, ".RData"))
+}else{
+  load(paste0(directory, "power_tests.RData"))
+  browser()
+}
 
   power_values
 }
