543d205b825b7e0ce18fa752f28259c58d5a2e5d
BenBarnard <ben_barnard@baylor.edu>
2017-06-01 17:29:26 -0500

    u[date

---
+++ src/Chaipitak2013.cpp
@@ -0,0 +1,20 @@
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+using namespace arma;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+List Chaipitak2013Stat(List x) {
+
+  return x;
+}
+
