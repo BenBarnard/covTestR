a84f1693f9375bd461b2e5800c1d1ea38cbd2f10
BenBarnard <ben_barnard@baylor.edu>
2017-06-12 15:17:09 -0500

    update

--- src/Ahmad2017.cpp
+++ src/Ahmad2017.cpp
@@ -6,132 +6,76 @@ using namespace arma;
 // source this function into an R session using the Rcpp::sourceCpp
 // function (or via the Source button on the editor toolbar). Learn
 // more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
+  //
+  //   http://www.rcpp.org/
+  //   http://adv-r.had.co.nz/Rcpp.html
 //   http://gallery.rcpp.org/
-//
-// [[Rcpp::depends(RcppArmadillo)]]
+  //
+  // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 double Ahmad2017Stat(List x) {
   int len = x.length();
-  arma::vec Eis(len);
-  arma::mat Eijs(len, len);
   double ntot = 0;
-  arma::mat pmat = x[1];
+  arma::mat pmat = x[0];
   double p = pmat.n_cols;
   arma::mat Apool(p, p);
-  List ni(len);
+  double Ei = 0;
+  double Eij = 0;
+  double ninv = 0;
+  double nijinv = 0;
 
   for(int i = 0; i < len; ++i){
-    arma::mat mats = x[i];
-    double E = 0;
-    int ns = mats.n_rows;
-    arma::mat diag(ns, ns);
-    diag.eye(ns, ns);
-    arma::mat J(ns, ns);
+    arma::mat mati = x[i];
+    double ni = mati.n_rows;
+    arma::mat diag(ni, ni);
+    diag.eye(ni, ni);
+    arma::mat J(ni, ni);
     J.fill(1);
-    arma::mat A = mats.t() * (diag - J / ns) * mats;
-    ntot += ns - 1;
+    arma::mat A = mati.t() * (diag - J / ni) * mati;
+    ntot += ni - 1;
     Apool += A;
-    ni[i] = ns;
+    double E = 0;
 
-    for(int k = 0; k < ns; ++k){
-      double e = 0;
-      for(int r = k + 1; r < ns; ++r){
-        for(int kp = r + 1; kp < ns; ++kp)
-          for(int rp = kp + 1; rp < ns; ++rp){
-              arma::mat x = mats.t();
-              e = as_scalar(pow((x.col(k) - x.col(r)).t() * (x.col(kp) - x.col(rp)), 2) +
-                pow((x.col(k) - x.col(kp)).t() * (x.col(rp) - x.col(rp)), 2) +
-                pow((x.col(k) - x.col(rp)).t() * (x.col(kp) - x.col(r)), 2));
-            }
-          }
-      E += e;
-      }
-
-    Eis[i] = 24 * E * pow(ns * (ns - 1) * (ns - 2) * (ns - 3), -1);
-
-
-  }
-
-
-  for(int i = 0; i < len; ++i){
-    for(int j =  i + 1; j < len; ++j){
-      arma::mat mati = x[i];
-      arma::mat matj = x[j];
-      arma::mat xi = mati.t();
-      arma::mat xj = matj.t();
-      double nis = ni[i];
-      double njs = ni[j];
-      double Eij = 0;
-      for(int k = 0; k < nis; ++k){
-        for(int r = 0; r < nis; ++r){
-          for(int l = 0; l < njs; ++l) {
-            for(int s = 0; s < njs; ++s){
-              double e = 0;
-              if (k == r | l == s) {
-                e = 0;
-              } else {
-                e = as_scalar(pow((xi.col(k) - xi.col(r)).t() * (xj.col(l) - xj.col(s)), 2));
-              }
-              Eij += e;
+    for(int k = 0; k < ni; ++k){
+      for(int r = k + 1; r < ni; ++r){
+        for(int kp = r + 1; kp < ni; ++kp){
+          for(int rp = kp + 1; rp < ni; ++rp){
+            E += as_scalar(pow((mati.row(k).t() - mati.row(r).t()).t() * (mati.row(kp).t() - mati.row(rp).t()), 2) +
+            pow((mati.row(k).t() - mati.row(kp).t()).t() * (mati.row(r).t() - mati.row(rp).t()), 2) +
+            pow((mati.row(k).t() - mati.row(rp).t()).t() * (mati.row(kp).t() - mati.row(r).t()), 2));;
             }
           }
         }
       }
 
-      Eijs(i, j) = Eij * pow(4 * nis * (nis - 1) * njs * (njs - 1), -1);
+    Ei += 2 * E * pow(ni * (ni - 1) * (ni - 2) * (ni - 3), -1);
+
+    ninv += pow(ni, -2);
+
+    for(int j =  i + 1; j < len; ++j){
+      arma::mat matj = x[j];
+      double nj = matj.n_rows;
+      double Eijs = 0;
+      for(int k = 0; k < ni; ++k){
+        for(int r = k + 1; r < ni; ++r){
+          for(int l = 0; l < nj; ++l) {
+            for(int s = l + 1; s < nj; ++s){
+              Eijs += as_scalar(pow((mati.row(k).t() - mati.row(r).t()).t() *
+              (mati.row(l).t() - mati.row(s).t()), 2));;
+            }
+          }
+        }
+      }
+
+      Eij += Eijs * pow(ni * (ni - 1) * nj * (nj - 1), -1);
+
+      nijinv += 2 * pow(ni * nj, -1);
     }
   }
 
   arma::mat pooledcov = Apool / ntot;
 
-  double doublesum = 0;
-  double singlesum = 0;
-  for(int i = 0; i < len; ++i){
-    double Ei = Eis[i];
-    singlesum += Ei;
-    for(int j = i + 1; j < len; ++j){
-      double Eij = Eijs[i];
-      doublesum += Eij;
-    }
-  }
+  double stat = (len - 1) * Ei - 2 * Eij * pow(4 * (pow(len - 1, 2) * ninv + nijinv), -0.5) * trace(pooledcov * pooledcov);
 
-  double a = 0;
-
-  for(int i = 0; i < len; ++i){
-    for(int j = i + 1; j < len; ++j){
-      double nsi = ni[i];
-      double nsj = ni[j];
-      double Eij = Eijs(i, j);
-      double dem = 0;
-      for(int k = i; k < j + 1; ++k){
-        dem += nsi * (nsi - 1) * nsj * (nsj - 1);
-      }
-      double asub = nsi * (nsi - 1) * nsj * (nsj - 1) * Eij * pow(dem, - 1);
-
-      a += asub;
-    }
-  }
-
-  double sing = 0;
-  double doub = 0;
-  for(int i = 0; i < len; ++i){
-    double nsi = ni[i];
-    sing += pow(nsi, -2);
-      for(int j = i + 1; j < len; ++j){
-        double nsj = ni[j];
-        doub += 2 * pow(nsi * nsj, -1);
-      }
-  }
-  double var2 = 4 * pow(a, 2) * (pow(len - 1, 2) * sing + doub);
-
-
-
-  double stat = a * ((len - 1) * singlesum - 2 * doublesum) *
-    pow(trace(pooledcov * pooledcov), -1) *
-    pow(var2, -0.5);
-
-  return a;
+  return stat;
 }
