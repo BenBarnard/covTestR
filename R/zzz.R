69952cd0331146e3ec7b48d5a1d7cbe1470cb6c0
benbarnard <benbarnard@usadir.usa.usouthal.edu>
2017-10-26 09:28:57 -0500

    update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,15 @@
+#' covTests
+#'
+#' Package Documentation
+#'
+#' @useDynLib covTestR
+#' @importFrom Rcpp sourceCpp
+NULL
+
+
+.onUnload <- function(libpath){
+  library.dynam.unload(
+    "covTestR", libpath
+  )
+}
 
